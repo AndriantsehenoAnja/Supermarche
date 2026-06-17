8f0f378f8aa582883a9d40dfdb4bcd3a26e4b72e
AndriantsehenoAnja <andriantsehenoanja926@gmail.com>
2026-06-17 11:35:34 +0300

    Refactor routes and controllers for product management; add VenteController for handling sales transactions and update views for better integration.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,3 +8,6 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->get('/produits', 'ProduitController::index');
 $routes->get('/produit/(:num)', 'ProduitController::show/$1');
+
+$routes->get('/achats', 'ProduitController::index');
+$routes->post('/vent/enregistrer', 'VenteController::enregistrer');
