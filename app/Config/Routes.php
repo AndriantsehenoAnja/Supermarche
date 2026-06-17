3d2a704367102ae0b751d3be9ebba4c7cb7483aa
Bryan <bryanmihajatiana101@gmail.com>
2026-06-17 11:09:21 +0300

    Update routes to direct to Caisse controller and add migration for Utilisateur table

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,12 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+/* $routes->get('/', 'Home::index');
 $routes->get('/produits', 'ProduitController::index');
 $routes->get('/produit/(:num)', 'ProduitController::show/$1');
+ */
+
+
+// Caisse
+$routes->get('/', 'Caisse::index');
+$routes->post('caisse/valider', 'Caisse::valider');
